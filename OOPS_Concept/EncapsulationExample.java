e99b3758956553781f4b5079728f6997d1ac94f8
Divya Rai <divyabhu7@gmail.com>
2022-08-29 11:57:40 +0530

    Encapsulation added

---
+++ OOPS_Concept/EncapsulationExample.java
@@ -0,0 +1,12 @@
+package OOPS_Concept;
+// Encapsulation is a process of wrapping code and data into a single unit
+
+public class EncapsulationExample {
+   private String name;
+   public String getName(){
+       return name;
+   }
+   public void setName(String name){
+       this.name=name;
+   }
+}
---
+++ OOPS_Concept/EncapsulationTest.java
@@ -0,0 +1,9 @@
+package OOPS_Concept;
+
+public class EncapsulationTest {
+    public static void main(String[] args) {
+        EncapsulationExample e=new EncapsulationExample();
+        e.setName("Divya Rai");
+        System.out.println(e.getName());
+    }
+}
